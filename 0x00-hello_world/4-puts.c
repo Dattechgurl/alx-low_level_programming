183c93f245e41e1b7b4fc4b17c451a34c96eb744
dattechgurl <sanni_afolashade@yahoo.com>
2023-02-16 13:31:57 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with put function
+ * Description: 'Using function put'
+ * return: 0(valid)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
