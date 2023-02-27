dd670f5c2fed928efe1aa7e52abc71402779a38e
dattechgurl <sanni_afolashade@yahoo.com>
2023-02-27 14:25:26 -0800

    Task 0

--- 0x05-pointers_arrays_string/main.h
+++ 0x05-pointers_arrays_string/main.h
@@ -1,6 +1,3 @@
-#include "main.h"
-#include <stdio.h>
-
 int _putchar(char c);
 
 void reset_to_98(int *n);
@@ -24,5 +21,3 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
-
-
