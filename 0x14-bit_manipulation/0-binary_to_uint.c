7dbdfad90bf64215dbb8de27c36fb04e8d905893
Dattechgurl <Brawphiat.123@gmail.com>
2023-04-09 13:45:43 -0700

    functions that convert dec

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,14 +11,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int val =0;
-int k = 0;
+int i = 0;
 if (b == NULL)
 return (0);
-while (b[k] == '0' || b[k] == '1')
+while (b[i] == '0' || b[i] == '1')
 {
 val <<= 1;
-val += b[k] - '0';
-k++;
+val += b[i]-'0';
+i++;
 }
-return (val);
+return val;
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
+
 
 /**
  * main - header files for prototypes
  * void: prototypes(void)
- *int: prototype(int) 
+ *int: prototype(int)
  */
+
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
